8bb7676bc6b905b20a87bf499a1b4b8df986d5bc
Vasyl Cherkes <cherkesva@gmail.com>
2026-02-23 15:54:03 +0200

    Add library_management module skeleton

---
+++ addons/library_management/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Library Management",
+    "version": "1.0",
+    "summary": "Manage library books and rentals",
+    "author": "Vasyl Cherkes",
+    "category": "Services",
+    "depends": ["base", "contacts"],
+    "data": [],
+    "installable": True,
+    "application": True,
+}
